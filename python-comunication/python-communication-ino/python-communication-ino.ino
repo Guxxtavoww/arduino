3375b4fe97521cdaf06aa962a6c02800d5c2b9dd
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-12-01 14:05:15 -0300

    Chore: project works

--- python-comunication/python-communication-ino/python-communication-ino.ino
+++ python-comunication/python-communication-ino/python-communication-ino.ino
@@ -11,9 +11,11 @@ void loop() {
     char command = Serial.read();
 
     if (command == 'L') {
-      digitalWrite(LED, HIGH);      
+      digitalWrite(LED, HIGH); 
+      Serial.println("LED Ligado"); // Send a command to Python     
     } else if (command == 'D') {
       digitalWrite(LED, LOW);
+      Serial.println("LED Desligado"); // Send a command to Python
     } else {
       Serial.println("Comando desconhecido");
     }
